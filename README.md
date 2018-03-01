43fb29e0a3004abe8749c9ca82feb962ce0160c3
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2018-03-01 23:59:57 +1300

    Initial commit; base app

---
+++ README.md
@@ -0,0 +1,4 @@
+Development:
+1. `npm install` to get dependencies.
+2. `npm start` to start server. Open http://localhost:8080.
+3. `npm run build` to create production output.
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require( 'path' );
+var webpack = require( 'webpack' );
+var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+
+module.exports = {
+    entry: path.resolve( __dirname, 'src', 'app' ),
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.resolve( __dirname, './' ),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /(\.css)$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    }, 
+                    {
+                        loader: 'css-loader'
+                    }
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Main',
+            template: 'index.hbs',
+        })
+    ],
+    resolve: {
+        alias: {
+            Components: path.resolve( __dirname, 'src', 'components' ),
+            Styles: path.resolve( __dirname, 'src', 'styles' ),
+        },
+        extensions: ['.js', '.jsx'],
+    },
+};
